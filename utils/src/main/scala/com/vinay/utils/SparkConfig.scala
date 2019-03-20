e6a4abd209b8a74221c0933277d9a6858ff5a1dc
vinay <vjayadev.bst@gmail.com>
2019-03-20 06:52:38 +0530

    SparkSession--LinuxMint

--- utils/src/main/scala/com/vinay/utils/SparkConfig.scala
+++ utils/src/main/scala/com/vinay/utils/SparkConfig.scala
@@ -10,7 +10,15 @@ import  org.apache.spark.sql.SparkSession
   * Created by ADMIN on 27-08-18.
   */
 trait SparkConfig {
-  val sparkSession = SparkSession.builder().getOrCreate()
+  /*val sparkSession = SparkSession.builder().getOrCreate()
   val Spark = SparkSession.builder().config("spark.sql.warehouse.dir","/home/hadoop/warehosue").enableHiveSupport().getOrCreate()
-  val sQLContext = sparkSession.sqlContext
+  val sQLContext = sparkSession.sqlContext*/
+
+  va sparkSession:SparkSession = SparkSession.builder().
+    config("spark.sql.warehouse.dir","/data/dev/warehouse/siu/dda_siu").
+    config("spark.shuffle.spill","false").
+    config("spark.rdd.compress","true").
+    config("spark.storage.memoryFraction","1").
+    config("saprk.shuffle.consolidateFiles","true").
+    config("spark")
 }
